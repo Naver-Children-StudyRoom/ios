8a30b590eedc81bfb1a99816f816dabfa21b657c
김주영 <rlawndud7@nate.com>
2022-01-15 16:47:12 +0900

    rxswift 추가 및 베이스 파일 추가

---
+++ Study_Room/Study_Room/ApiRequest.swift
@@ -0,0 +1,13 @@
+//
+//  ApiRequest.swift
+//  Study_Room
+//
+//  Created by Joo Young Kim on 2022/01/15.
+//
+
+import SwiftyJSON
+import Alamofire
+
+class ApiRequest {
+    public static let shared = ApiRequest()
+}
