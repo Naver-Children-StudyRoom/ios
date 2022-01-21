a60c1f02168f41ada55778c235525917eeb9b736
김주영 <rlawndud7@nate.com>
2022-01-21 10:08:44 +0900

    CleanroomLogger를 적용하기 위해 BootModule 구축 및 Log 구현

--- Study_Room/Study_Room/Logger/Logger.swift
+++ Study_Room/Study_Room/Logger/Logger.swift
@@ -8,46 +8,31 @@
 import Foundation
 import CleanroomLogger
 
-public func log(severity: LogSeverity, msg: String, function: String = #function, filePath: String = #file, fileLine: Int = #line, errorType: String? = nil, errorCode: Int? = nil) {
+public func log(severity: LogSeverity, msg: String, function: String = #function, filePath: String = #file, fileLine: Int = #line, errorType: ErrorType? = nil, errorCode: Int? = nil) {
     cleanRoomLogWrapper(severity: severity, message: msg, function: function, filePath: filePath, fileLine: fileLine, errorType: errorType, errorCode: errorCode)
 }
 
-public func debugLog(_ msg: String, function: String = #function, filePath: String = #file, fileLine: Int = #line, errorType: String? = nil, errorCode: Int? = nil) {
-    Log.debug?.trace()
+public func debugLog(_ msg: String, function: String = #function, filePath: String = #file, fileLine: Int = #line, errorType: ErrorType? = nil, errorCode: Int? = nil) {
     cleanRoomLogWrapper(severity: .debug, message: msg, function: function, filePath: filePath, fileLine: fileLine, errorType: errorType, errorCode: errorCode)
 }
 
-private func cleanRoomLogWrapper(severity: LogSeverity, message: String, function: String = #function, filePath: String = #file, fileLine: Int = #line, errorType: String? = nil, errorCode: Int? = nil) {
+public func infoLog(_ msg: String, function: String = #function, filePath: String = #file, fileLine: Int = #line, errorType: ErrorType? = nil, errorCode: Int? = nil) {
+    cleanRoomLogWrapper(severity: .info, message: msg, function: function, filePath: filePath, fileLine: fileLine, errorType: errorType, errorCode: errorCode)
+}
+
+public func warningLog(_ msg: String, function: String = #function, filePath: String = #file, fileLine: Int = #line, errorType: ErrorType? = nil, errorCode: Int? = nil) {
+    cleanRoomLogWrapper(severity: .warning, message: msg, function: function, filePath: filePath, fileLine: fileLine, errorType: errorType, errorCode: errorCode)
+}
+
+public func errorLog(_ msg: String, function: String = #function, filePath: String = #file, fileLine: Int = #line, errorType: ErrorType? = nil, errorCode: Int? = nil) {
+    cleanRoomLogWrapper(severity: .error, message: msg, function: function, filePath: filePath, fileLine: fileLine, errorType: errorType, errorCode: errorCode)
+}
+
+private func cleanRoomLogWrapper(severity: LogSeverity, message: String, function: String = #function, filePath: String = #file, fileLine: Int = #line, errorType: ErrorType? = nil, errorCode: Int? = nil) {
+    
     if let errorType = errorType {
-        Log.channel(severity: severity)?.message(message, function: function, filePath: filePath, fileLine: fileLine, errorType: errorType, errorCode: errorCode)
+        Log.channel(severity: severity)?.message(message, function: function, filePath: filePath, fileLine: fileLine, errorType: errorType.rawValue, errorCode: errorCode)
     } else {
         Log.channel(severity: severity)?.message(message, function: function, filePath: filePath, fileLine: fileLine)
-    }
-}
-
-extension LogChannel {
-    
-    /// 에러타입이 추가된 로그 메세지를 남긴다.
-    ///
-    /// - Attention: warning, error 레벨이거나 errorType 값이 존재하는 경우에는 Nelo에도 Log를 남긴다.
-    func message(_ msg: String, function: String = #function, filePath: String = #file, fileLine: Int = #line, errorType: String, errorCode: Int?) {
-        let msgWithErrCode = msg.append(errorType: errorType, errorCode: errorCode)
-        
-        message(msgWithErrCode, function: function, filePath: filePath, fileLine: fileLine)
-    }
-}
-
-extension String {
-    
-    private var messageDivider: String { return "__errordivider__" }
-    private var errorDivider: String { return "_" }
-    
-    // {스트링}__errordivider__{에러타입}_{에러코드}
-    fileprivate func append(errorType: String, errorCode: Int? = nil) -> String {
-        let message = self + messageDivider + errorType
-        
-        guard let errorCode = errorCode else { return message }
-        
-        return message + errorDivider + String(errorCode)
     }
 }
--- Study_Room/Study_Room/Study_RoomApp.swift
+++ Study_Room/Study_Room/Study_RoomApp.swift
@@ -9,6 +9,14 @@ import SwiftUI
 
 @main
 struct Study_RoomApp: App {
+    
+    /// 앱에서 처음에 기본 세팅해야하는 부분에 대해서 이곳에서 처리
+    /// UIKit에서 Appdelegate 가 이부분이라고 생각하면됨
+    /// 기본 configuration들을 여기서 관리하자 -> 모듈화 좋은 생각 
+    init() {
+        BootLoader.runBootLoaderModules()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
