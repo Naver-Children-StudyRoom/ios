33c3f7e2dcd7945301797081b2f17392128cb37f
김주영 <rlawndud7@nate.com>
2022-01-24 20:40:03 +0900

    sceneDelegate 적용

--- Study_Room/Study_Room/Study_RoomApp.swift
+++ Study_Room/Study_Room/Study_RoomApp.swift
@@ -22,6 +22,9 @@ struct Study_RoomApp: App {
     // appDelegate 적용
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
+    // sceneDelegate 적용
+    @Environment(\.scenePhase) private var scenePhase
+    
     /// 앱에서 처음에 기본 세팅해야하는 부분에 대해서 이곳에서 처리
     /// UIKit에서 Appdelegate 가 이부분이라고 생각하면됨
     /// 기본 configuration들을 여기서 관리하자 -> 모듈화 좋은 생각
@@ -33,5 +36,26 @@ struct Study_RoomApp: App {
         WindowGroup {
             ContentView()
         }
+        .onChange(of: scenePhase) { (newScenePhase) in
+            switch newScenePhase {
+            case .active:
+                AppManager.shared.isActive = true
+                debugLog("앱 활성화")
+            case .inactive:
+                infoLog("앱이 \(AppManager.shared.isActive ? "background" : "foreground")에 진입합니다.")
+            case .background:
+                AppManager.shared.isActive = false
+                debugLog("앱 비활성화")
+            @unknown default:
+                errorLog("의도치 않은 상태")
+            }
+        }
     }
+}
+
+/// Foreground -> Background 또는 Background -> Foreground 를 알아내기 위한 싱글톤 객체
+class AppManager {
+    var isActive = false
+    static var shared = AppManager()
+    private init() {}
 }
