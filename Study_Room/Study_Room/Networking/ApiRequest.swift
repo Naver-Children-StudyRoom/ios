ba284ddccccbc9ddeede6fa307c60930dcccd60f
김주영 <rlawndud7@nate.com>
2022-01-25 21:38:45 +0900

    베이스 코드 추가

--- Study_Room/Study_Room/Networking/ApiRequest.swift
+++ Study_Room/Study_Room/Networking/ApiRequest.swift
@@ -8,6 +8,118 @@
 import SwiftyJSON
 import Alamofire
 
-class ApiRequest {
+open class ApiRequest {
     public static let shared = ApiRequest()
+    
+    /// JSON response를 받는 API Request
+    /// - Parameters:
+    ///   - api: 사용할 API. relative path만 전달하는 경우는 `UrlBuilder`에서 api gateway 주소와 조합해서 full url을 생성합니다. (ex : "members/{memberId}/documents")
+    ///   - apiType: `APIGatewayType`에서 해당되는 타입을 선택한다. 기본값은 `live`
+    ///   - method: `HTTPMethod`. 기본값은 `get`
+    ///   - parameters: API URL에 추가하는 query parameter dictionary
+    ///   - requestParameters: HTTP Request body에 추가하는 parameter dictionary
+    ///   - customParameters: HTTP Request body에 추가하지만 dictionary가 아닌 String이나 Array 타입의 파라미터. 일반적으로 잘 사용하지 않는다.
+    ///   - headers: 헤더
+    ///   - queue: completion handler가 호출될 Queue
+    ///   - retrier: API 실패시 재시도 처리를 해주는 `RequestRetryHandler`
+    ///   - success: 성공 클로저
+    ///   - failure: 실패 클로저
+    /// - Returns: `DataRequest`
+    @discardableResult
+    open class func requestJSON(
+        api: String,
+        method: HTTPMethod = .get,
+        parameters queryParameters: Parameters? = nil,
+        requestParameters: Parameters? = nil,
+        headers: HTTPHeaders? = nil,
+        queue: DispatchQueue? = nil,
+        retrier: RequestInterceptor? = nil,
+        success: ((JSON) -> Void)? = nil,
+        failure: ((NSError?) -> Void)? = nil) -> DataRequest {
+        return requestJSON(api: api, method: method, parameters: queryParameters, requestParameters: requestParameters, headers: headers, queue: queue, retrier: retrier, completion: { result in
+            switch result {
+            case .success(let json): success?(json)
+            case .failure(let error): failure?(error as NSError)
+            }
+        })
+    }
+    
+    /// JSON response를 받는 API Request
+    /// - Parameters:
+    ///   - api: 사용할 API. relative path만 전달하는 경우는 `UrlBuilder`에서 api gateway 주소와 조합해서 full url을 생성합니다. (ex : "members/{memberId}/documents")
+    ///   - apiType: `APIGatewayType`에서 해당되는 타입을 선택한다. 기본값은 `live`
+    ///   - method: `HTTPMethod`. 기본값은 `get`
+    ///   - parameters: API URL에 추가하는 query parameter dictionary
+    ///   - requestParameters: HTTP Request body에 추가하는 parameter dictionary
+    ///   - customParameters: HTTP Request body에 추가하지만 dictionary가 아닌 String이나 Array 타입의 파라미터. 일반적으로 잘 사용하지 않는다.
+    ///   - headers: 헤더
+    ///   - queue: completion handler가 호출될 Queue
+    ///   - retrier: API 실패시 재시도 처리를 해주는 `RequestRetryHandler`
+    ///   - completion: completion closure
+    /// - Returns: `DataRequest`
+    @discardableResult
+    open class func requestJSON(
+        api: String,
+        method: HTTPMethod = .get,
+        parameters queryParameters: Parameters? = nil,
+        requestParameters: Parameters? = nil,
+        headers: HTTPHeaders? = nil,
+        queue: DispatchQueue? = nil,
+        retrier: RequestInterceptor? = nil,
+        completion: ((Result<JSON, Error>) -> Void)?) -> DataRequest {
+        let (dataRequest, sessionManagerID) = request(api: api, method: method, parameters: queryParameters, requestParameters: requestParameters, headers: headers, queue: queue, retrier: retrier)
+        
+        dataRequest.responseJSON(queue: queue ?? .main) { (response: AFDataResponse<Any>) -> Void in
+            if let result = response.value, response.error == nil {
+                Dependency.record(apiType: apiType.rawValue, api: api, method: method.rawValue)
+                completion?(.success(JSON(result)))
+            } else {
+                let error = response.parsedError
+                shared.tryLoginClosure?(api, error)
+                recordError(response: response, method: method, queryParameters: queryParameters, requestParameters: requestParameters, error: error)
+                
+                Dependency.record(apiType: apiType.rawValue, api: api, method: method.rawValue, error: error)
+                completion?(.failure(error))
+            }
+            
+            shared.removeSessionManager(id: sessionManagerID)
+        }
+        
+        return dataRequest
+    }
+    
+    /// Alamofire DataRequest 생성
+    ///
+    /// - Parameters:
+    ///   - api: 사용할 API. relative path만 전달하는 경우는 `UrlBuilder`에서 api gateway 주소와 조합해서 full url을 생성합니다. (ex : "members/{memberId}/documents")
+    ///   - apiType: `APIGatewayType`에서 해당되는 타입을 선택한다. 기본값은 `live`
+    ///   - method: `HTTPMethod`. 기본값은 `get`
+    ///   - parameters: API URL에 추가하는 query parameter dictionary
+    ///   - requestParameters: API Request body에 추가하는 parameter dictionary
+    ///   - customParameters: dictionary가 아닌 String이나 Array 타입의 파라미터. 일반적으로 잘 사용하지 않는다.
+    ///   - headers: 헤더
+    ///   - queue: completion handler가 호출될 Queue
+    ///   - retrier: API 실패시 재시도 처리를 해주는 `RequestRetryHandler`
+    open class func request(
+        api: String,
+        method: HTTPMethod = .get,
+        parameters queryParameters: Parameters? = nil,
+        requestParameters: Parameters? = nil,
+        headers: HTTPHeaders? = nil,
+        queue: DispatchQueue? = nil,
+        retrier: RequestInterceptor? = nil) -> (DataRequest, String) {
+        let url = api.isValidUrlHeader ? api : UrlBuilder.createUrl(relativePath: api, apiType: apiType, parameters: queryParameters)
+        debugLog("API Request :: url = \(url)")
+        
+        let sessionManager = Session.createInstance(retrier: retrier)
+        let sessionManagerID = UUID().uuidString
+        shared.add(sessionManager: sessionManager, id: sessionManagerID)
+        
+        let encoding: ParameterEncoding = {
+            guard let encoding = customParameters?.encoding else { return requestParameters == nil ? URLEncoding.default : JSONEncoding.default }
+            return encoding
+        }()
+        let dataRequest = sessionManager.request(url, method: method, parameters: requestParameters ?? customParameters?.asParameters, encoding: encoding, headers: createHeaders(with: headers, apiType: apiType)).validate()
+        return (dataRequest, sessionManagerID)
+    }
 }
---
+++ Study_Room/Study_Room/Networking/URLBuilder.swift
@@ -0,0 +1,98 @@
+//
+//  URLBuilder.swift
+//  Study_Room
+//
+//  Created by JooYoung Kim on 2022/01/25.
+//
+
+import Foundation
+
+public class UrlBuilder {
+    
+    static let consumer = "selectiveApp"
+    
+    /// 현재 server phase 설정값에 기반한 API 주소를 생성하고 HMAC encryption을 한다.
+    ///
+    /// - Parameters:
+    ///   - relativePath: url string. 스트링 앞에 /가 있던 없던간에 관계없이 동작한다.
+    ///   - apiType: 태그검색은 .tagSearch 명시. 미입력시 GP gateway로 동작한다.
+    ///   - parameters: paramter dictionary
+    /// - Returns: 생성된 url String
+    public class func createUrl(relativePath: String, apiType: APIGatewayType = .gp, parameters: [String: Any]? = nil) -> String {
+        var url = apiPath(apiType: apiType) + validate(relativePath: relativePath).apiEncoded
+        
+        if let param = parameters {
+            let query = queryString(with: param)
+            url = url.appending(query)
+        }
+        
+        switch apiType {
+        case .store: return url.hmacEncrypt
+        case .prism: return url.hmacEncryptForPrism
+        default:     return url.hmacEncryptForLive
+        }
+    }
+    
+    /// next key parameter 생성
+    class func createParameter(nextKey: Int?, apiType: APIGatewayType = .live) -> [String: Any] {
+        let countField: String = apiType == .live ? "pageSize" : "count"
+        var parameters: [String: Any] = [countField: 20]
+        
+        if let nextKey = nextKey {
+            let nextField: String = apiType == .live ? "page" : "next"
+            parameters[nextField] = nextKey
+        }
+        
+        return parameters
+    }
+}
+
+extension UrlBuilder {
+    
+    fileprivate class func apiPath(apiType: APIGatewayType) -> String {
+        return "https://\(apiType.gatewayPrefix)apis.naver.com/" + consumer + "/" + apiType.provider + "/"
+    }
+    
+    // 주소 앞에 /가 붙어있으면 제거한다.
+    fileprivate class func validate(relativePath: String) -> String {
+        if relativePath.firstCharacterAsString == "/" {
+            return String(relativePath.dropFirst())
+        } else {
+            return relativePath
+        }
+    }
+    
+    /// API에 URL Query 파라미터를 사용할 때, Dictionary를 Query로 파싱하는 메소드
+    public class func queryString(with param: [String: Any], startsWithQuestionMark: Bool = true) -> String {
+        var query = ""
+        
+        for key in param.keys {
+            guard let value = param[key] else { continue }
+            
+            if query.count == 0 {
+                query = startsWithQuestionMark ? "?" : ""
+            } else {
+                query = query.appending("&")
+            }
+            
+            if let arrayValue = value as? QueryConvertable {
+                query = query.appending(arrayValue.queryString(key: key))
+            } else {
+                query = query.appending("\(key)=\((value as? String)?.apiParameterEncoded ?? value)")
+            }
+        }
+        
+        return query
+    }
+}
+
+protocol QueryConvertable {
+    func queryString(key: String) -> String
+}
+
+// Element가 .description을 가지는 Array
+extension Array: QueryConvertable where Element: CustomStringConvertible {
+    public func queryString(key: String) -> String {
+        return map({ return "\(key)=\($0.description.apiParameterEncoded)"}).joined(separator: "&")
+    }
+}
