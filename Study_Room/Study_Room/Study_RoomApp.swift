a7608a6cbacc77bf94afae0d49dcc55149e693da
김주영 <rlawndud7@nate.com>
2022-01-30 10:46:15 +0900

    시작 위치 변경

--- Study_Room/Study_Room/Study_RoomApp.swift
+++ Study_Room/Study_Room/Study_RoomApp.swift
@@ -34,7 +34,7 @@ struct Study_RoomApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
         .onChange(of: scenePhase) { (newScenePhase) in
             switch newScenePhase {
