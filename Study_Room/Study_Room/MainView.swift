34830b0110450e27f67c380b856ea2db9bbba181
김주영 <rlawndud7@nate.com>
2022-03-18 14:24:17 +0900

    [#8,#9] 메인화면 구현을 위한 베이스 코드 작성

--- Study_Room/Study_Room/MainView.swift
+++ Study_Room/Study_Room/MainView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 /// 메인 화면으로 쓰자
 struct MainView: View {
+    var loginCheck: Bool = false // 로그인되있는지 여부 체크하고 로그인되있으면 로그인메인화면 아니면 비로그인메인화면 
     var body: some View {
         VStack {
             NavigationView {
@@ -18,6 +19,11 @@ struct MainView: View {
             }
             Text("Hello, world!")
                 .padding()
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: {
+                debugLog("config 정보 로드할 부분")
+            })
         }
     }
 }
