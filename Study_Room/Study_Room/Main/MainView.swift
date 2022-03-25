3f9726c2bed93ffd740fec887c627ed574cd6a38
김주영 <rlawndud7@nate.com>
2022-03-25 09:46:33 +0900

    [#8] TODO

--- Study_Room/Study_Room/Main/MainView.swift
+++ Study_Room/Study_Room/Main/MainView.swift
@@ -21,6 +21,9 @@ struct MainView: View {
         GeometryReader { geometry in
             if userManager.loginCheck ?? true {
                 TabView(selection: $viewModel.currentTab) {
+//                    Text("test") // TODO: 나중에 로그인 했을 때 처음에 나의 독서실 요약, 추천독서실, 마이플래너 미리보기가 구현된 View를 출력할 예정
+//                        .frame(width: geometry.size.width, height: geometry.size.height)
+//                        .tag(Tab.Home)
                     ServiceIntroDetailView()
                         .frame(width: geometry.size.width, height: geometry.size.height)
                         .tag(Tab.ServiceInfo)
--- Study_Room/Study_Room/Main/MainViewModel.swift
+++ Study_Room/Study_Room/Main/MainViewModel.swift
@@ -9,6 +9,7 @@ import Foundation
 
 class MainViewModel: ObservableObject {
     @Published var currentTab: Tab = .ServiceInfo
+//    @Published var currentTab: Tab = .Home // TODO: 나중에 메인화면 개발 완료되면 변경할 예정
 }
 
 // MARK: - TAB ITEM CASE
@@ -17,4 +18,5 @@ enum Tab: String {
     case StudyExplore = "studyExplore"
     case Planner = "planner"
     case Mypage = "mypage"
+    case Home = "home"
 }
