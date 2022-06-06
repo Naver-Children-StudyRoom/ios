a9aad909d1c71f50fc7e258537133649d738ea80
김주영 <rlawndud7@nate.com>
2022-06-06 16:30:28 +0900

    [#2] 회원가입 API 뼈대

--- Study_Room/Study_Room/Member/Signup/SignupView.swift
+++ Study_Room/Study_Room/Member/Signup/SignupView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct SignupView: View {
+    @ObservedObject var viewModel: SignUpViewModel = SignUpViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            NavigationBarView(isShow: .constant(false), title: "회원가입")
+                .frame(height: 54)
+                .frame(maxWidth: .infinity)
+            Spacer()
+            HStack {
+                Text("Email : ")
+                TextField("Email", text: $viewModel.userEmail)
+                    .frame(width: 100.0, height: 10.0)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
+            }
+            HStack {
+                Text("Password : ")
+                TextField("Password", text: $viewModel.userPassword)
+                    .frame(width: 100.0, height: 10.0)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
+            }
+            HStack {
+                Text("name : ")
+                TextField("name", text: $viewModel.userName)
+                    .frame(width: 100.0, height: 10.0)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 10).strokeBorder())
+            }
+            Button {
+                viewModel.requestSignUpUser()
+                debugLog("회원가입")
+            } label: {
+                Text("회원가입")
+            }
+
+            Spacer()
+        }
     }
 }
 
