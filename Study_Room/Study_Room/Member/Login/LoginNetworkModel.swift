86296bd186f571922e006b2779c0686a40204d34
김주영 <rlawndud7@nate.com>
2022-06-06 15:39:17 +0900

    [#3] 로그인 API연동

--- Study_Room/Study_Room/Member/Login/LoginNetworkModel.swift
+++ Study_Room/Study_Room/Member/Login/LoginNetworkModel.swift
@@ -7,31 +7,52 @@
 
 import Foundation
 import SwiftyJSON
+import Alamofire
 
 class LoginNetworkModel {
-    func requestLoginUser(completion: @escaping ((Result<UserModel, Error>) -> Void)) {
+    func requestLoginUser(userEmail: String, userPassword: String, completion: @escaping ((Result<UserModel, Error>) -> Void)) {
         let api = "login"
         
-        APIRequest.requestJSON(api: api, method: .post, completion: { result in
-            if let json = result.success, let user = UserModel(json: json) {
-                self.setCurrentUserInfo(json: json)
-                completion(.success(user))
-            } else {
-                completion(.failure(result.error ?? CommonError.failedToFetch))
+        let param: [String: Any] = [
+            "email": userEmail,
+            "password": userPassword
+        ]
+        
+        let request = APIRequest.request(api: api, method: .post, requestParameters: param).0
+        
+        request.response(completionHandler: { [weak self] response in
+            guard let accessToken = self?.fetchUserInfo(from: response), let data = response.value, response.error == nil else {
+                let error = response.parsedError
+                debugLog("Api Error: \(error)")
+                completion(.failure(error))
+                return
             }
+            
+            debugLog("로그인에 성공하였습니다. 현재 로그인한 유저의 accessToken값은 다음과 같습니다: \(accessToken)")
+            let json = JSON(data ?? Data())
+            let user = UserModel(json: json)
+            self?.setCurrentUserInfo(json: json, accessToken: accessToken)
+            completion(.success(user ?? UserModel()))
         })
     }
     
-    func setCurrentUserInfo(json: JSON) {
+    private func fetchUserInfo(from response: AFDataResponse<Data?>) -> String? {
+        guard let accessToken = response.response?.allHeaderFields["Authorization"] as? String, !accessToken.isBlank else {
+            warningLog("로그인 API에서 토큰 획득 실패.\n\(response)")
+            return nil
+        }
+        return accessToken
+    }
+    
+    func setCurrentUserInfo(json: JSON, accessToken: String) {
         guard let email = json["email"].string,
               let nickname = json["nickname"].string,
-              let accessToken = json["accessToken"].string,
-              let refreshToken = json["refreshToken"].string else { return }
+              let profileImageUri = json["profileImageUri"].string else { return }
         
         UserDefaults.standard.set(email, forKey: "email")
         UserDefaults.standard.set(nickname, forKey: "nickname")
         UserDefaults.standard.set(accessToken, forKey: "accessToken")
-        UserDefaults.standard.set(refreshToken, forKey: "refreshToken")
+        UserDefaults.standard.set(profileImageUri, forKey: "profileImageUri")
         UserDefaults.standard.set(true, forKey: "isLogin")
     }
 }
